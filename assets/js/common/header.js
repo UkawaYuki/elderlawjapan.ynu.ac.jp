8ae34bc8514d0f0580939cda6f50cfcfb730f7fb
Yuki Ukawa <50737127+UkawaYuki@users.noreply.github.com>
2021-03-05 21:23:45 +0900

    見た目実装

--- assets/js/common/header.js
+++ assets/js/common/header.js
@@ -1,8 +1,10 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   const burger = document.querySelector('#burger')
   const navi = document.querySelector('.menuList')
+  const block = document.querySelector('header .bottomBlock')
   burger.addEventListener('click', function () {
     burger.classList.toggle('open')
     navi.classList.toggle('open')
+    block.classList.toggle('open')
   })
 })
